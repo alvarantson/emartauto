0f608e97b1fbf7b8e86ecb5d457ffd063b1e5d8b
alvarantson <alvarantson@gmail.com>
2021-02-26 16:27:49 +0200

    rmp
    rmp update

--- rmp/views.py
+++ rmp/views.py
@@ -15,7 +15,12 @@ def rmp(request):
 		{"name": "HAMBURGER 2020", "link": "1WwRI6B5yYPAMrtMyDc70ihF3RLi2rTknjXC8WqeB6vs"},
 		{"name": "EMART AUTO 2020", "link": "13-p3KhBsqOGTPWCdij18R8x-aBJiezDweVXT_kEFElM"},
 		{"name": "AUTOPOOD 2020", "link": "1rFAjFjZLvfO2LEIiVNMfD2bKQ8bpgSwsGg4UC8OJbMI"},
-		{"name": "uus dokument", "link": "1oVqKu1NyGbplMXAnMkkWU0oRW2OysVYMQ7K922i7NKA"}
+		{"name": "uus dokument", "link": "1oVqKu1NyGbplMXAnMkkWU0oRW2OysVYMQ7K922i7NKA"},
+		{"name": "VALKLA 2021", "link": "1vvZFqZQ9MuaXIAn1dqd-OGiQePG_I_gqGVrmEZtrF_g"},
+		{"name": "KUUSALU 2021", "link": "1QtUaDGjPSkXF_Y-AN7RezJaBIXTYozjVCzDU3sAJntU"},
+		{"name": "HAMBURGER 2021", "link": "1eupNMt8Qxh0CKuR7PdHYt8YFpPrw9v-3wGK1yMY7_xo"},
+		{"name": "EMART AUTO 2021", "link": "1-EseiSl1ZRUSGTYQwKRWUtzAxJ3863rjI1EM06avyEI"},
+		{"name": "AUTOPOOD 2021", "link": "1iCFsU3ferOu-zlhU7opd5d53DOzMzPh-TlWq4WEuVmU"}
 		]
 
 	return render(request, "rmp.html", {
